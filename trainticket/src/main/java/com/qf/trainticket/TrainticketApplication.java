48ddaedc14a0779eccb3eaf390b00d87c706f84f
Administrator <1534136311@qq.com>
2019-01-21 18:46:49 +0800

    不懂的地方就问,配置方面不全，你们需要用的什么，都需要自己添加进去

---
+++ trainticket/src/main/java/com/qf/trainticket/TrainticketApplication.java
@@ -0,0 +1,14 @@
+package com.qf.trainticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainticketApplication.class, args);
+	}
+
+}
+
