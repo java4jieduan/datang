fe3a3f6a0f26abe4186820ec8a93d1f06143b5f5
Administrator <1534136311@qq.com>
2019-01-21 18:38:50 +0800

    不懂的地方就问,配置方面不全，你们需要用的什么，都需要自己添加进去

---
+++ gerenzhongxin/src/main/java/com/qf/gerenzhongxin/GerenzhongxinApplication.java
@@ -0,0 +1,14 @@
+package com.qf.gerenzhongxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenzhongxinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenzhongxinApplication.class, args);
+	}
+
+}
+
