b92b8588cba8f412f54205d0e2c3e5b4498714fe
Administrator <1534136311@qq.com>
2019-01-21 18:36:20 +0800

    不懂的地方就问,配置方面不全，你们需要用的什么，都需要自己添加进去

---
+++ flyticket/src/main/java/com/qf/flyticket/FlyticketApplication.java
@@ -0,0 +1,14 @@
+package com.qf.flyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyticketApplication.class, args);
+	}
+
+}
+
