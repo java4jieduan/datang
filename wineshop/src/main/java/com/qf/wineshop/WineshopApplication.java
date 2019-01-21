a835543051408541696ec89916aac94c50c246d6
Administrator <1534136311@qq.com>
2019-01-21 18:48:35 +0800

    不懂的地方就问,配置方面不全，你们需要用的什么，都需要自己添加进去

---
+++ wineshop/src/main/java/com/qf/wineshop/WineshopApplication.java
@@ -0,0 +1,14 @@
+package com.qf.wineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineshopApplication.class, args);
+	}
+
+}
+
