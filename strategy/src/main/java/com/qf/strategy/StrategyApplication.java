1595420fcaab4d5d361a6f8d653c515a11527b24
wenyingsong <1653354085@qq.com>
2019-01-23 22:42:42 +0800

    数据库用的是xml配置文件 查询使用了redis

--- strategy/src/main/java/com/qf/strategy/StrategyApplication.java
+++ strategy/src/main/java/com/qf/strategy/StrategyApplication.java
@@ -1,9 +1,12 @@
 package com.qf.strategy;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.qianfeng.common","com.qf"})
 public class StrategyApplication {
 
 	public static void main(String[] args) {
